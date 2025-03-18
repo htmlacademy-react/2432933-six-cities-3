7f9d1494e0f9ac654a726461ba7bbc776a55d5e3
Maxim <farcdoo@gmail.com>
2025-03-18 18:58:30 +0300

    fix: чинит build

--- src/pages/utils.ts
+++ src/pages/utils.ts
@@ -15,9 +15,12 @@ const sortOffers: Record<SortType, SortFunction> = {
   [SortType.Popular]: (offers) => [...offers],
   [SortType.PriceLowToHigh] : (offers) => [...offers].sort((firstOffer, secondOffer) => firstOffer.price - secondOffer.price),
   [SortType.PriceHighToLow] : (offers) => [...offers].sort((firstOffer, secondOffer) => secondOffer.price - firstOffer.price),
-  [SortType.TopRatedFirst]  : (offers) => [...offers].sort((firstOffer, secondOffer) => secondOffer.rating - firstOffer.rating),
+  [SortType.TopRatedFirst] : (offers) => [...offers].sort((firstOffer, secondOffer) => secondOffer.rating - firstOffer.rating),
 };
 
-const sortingOffers = (offers: TypePlace[], key: SortType) => sortOffers[key](offers);
+const sortingOffers = (offers: TypePlace[], key: string) => {
+  const sortType = key as SortType;
+  return sortOffers[sortType](offers);
+};
 
 export {filtersOffers, sortingOffers};
