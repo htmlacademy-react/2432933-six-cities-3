5cafc9db8024fc6bd8d89a2e293fbf62dd93b965
Maxim <farcdoo@gmail.com>
2025-04-03 19:45:09 +0300

    feat: Оптипизация page

--- src/components/locations-list/locations-list.tsx
+++ src/components/locations-list/locations-list.tsx
@@ -1,28 +1,34 @@
 import clsx from 'clsx';
 import { cities } from '../const';
+import { useAppSelector, useAppDispatch } from '../../hooks/use-app-redux/use-app-redux';
+import { setCity } from '../../store/offers/offers-reducer';
 
-type LocationsProps = {
-  cityName : string;
-  handleFilter: (city: string) => void;
- };
 
-const LocationsList = ({cityName, handleFilter} :LocationsProps) => (
-  <div className="tabs">
-    <section className="locations container">
-      <ul className="locations__list tabs__list">
-        {cities.map((city) => (
-          <li className="locations__item" key={city}>
-            <a
-              className={clsx('locations__item-link tabs__item', {'tabs__item--active': cityName === city})}
-              onClick={() => handleFilter(city)}
-            >
-              <span>{city}</span>
-            </a>
-          </li>
-        ))}
-      </ul>
-    </section>
-  </div>
-);
+const LocationsList = () => {
+  const dispatch = useAppDispatch();
+  const cityName = useAppSelector((state) => state.offers.city);
 
+  const handleFilter = (city: string) => {
+    dispatch(setCity(city));
+  };
+
+  return(
+    <div className="tabs">
+      <section className="locations container">
+        <ul className="locations__list tabs__list">
+          {cities.map((city) => (
+            <li className="locations__item" key={city}>
+              <a
+                className={clsx('locations__item-link tabs__item', {'tabs__item--active': cityName === city})}
+                onClick={() => handleFilter(city)}
+              >
+                <span>{city}</span>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
+};
 export default LocationsList;
--- src/pages/main-page/cities/cities-places.tsx
+++ src/pages/main-page/cities/cities-places.tsx
@@ -1,22 +1,23 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import PlacesList from '../../../components/places-list/places-list';
 import PlacesSorting from '../places-sorting/places-sorting';
-import { TypePlace } from '../../../types/place-type/place-type';
 import CitiesMap from '../../../components/cities-map/cities-map';
+import { useAppSelector } from '../../../hooks/use-app-redux/use-app-redux';
+import { offersSelector } from '../../../store/offers/offers-selector';
+import EmptyPlacesList from './empty-places-list';
 
-
-type PlacesListProps = {
-  places: TypePlace[];
-  cityName: string;
-};
-
-
-const CitiesPlaces = ({ places, cityName } :PlacesListProps) => {
+const CitiesPlaces = () => {
+  const places = useAppSelector(offersSelector);
+  const cityName = useAppSelector((state) => state.offers.city);
   const [activeCard, setActiveCard] = useState<string | null>(null);
 
-  const handleActiveCardChange = (id: string | null) => {
+  const handleActiveCardChange = useCallback((id: string | null) => {
     setActiveCard(id);
-  };
+  }, []);
+
+  if(places.length === 0){
+    return <EmptyPlacesList />;
+  }
 
   return(
     <div className="cities__places-container container">
