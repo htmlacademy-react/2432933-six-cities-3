9b4c07d7805d727e460862bf14801fc64a26ed31
Maxim <farcdoo@gmail.com>
2025-04-17 17:26:25 +0300

    fix: test

--- src/hooks/use-toggle-favorite.ts
+++ src/hooks/use-toggle-favorite.ts
@@ -1,16 +1,16 @@
+import { useNavigate } from 'react-router-dom';
 import { useAppSelector, useAppDispatch } from './use-app-redux/use-app-redux';
 import { AppRoute } from '../components/const';
 import { updateFavoriteStatus } from '../services/api-action/favorite-action';
-import { redirectToRoute } from '../store/redirect-to-route';
-
 
 const useToggleFavorite = (id: string, isFavorite: boolean) => {
   const isAuth = useAppSelector((state) => state.authStatus.isAuth);
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   const handleFavoriteClick = () => {
     if (!isAuth) {
-      return dispatch(redirectToRoute(AppRoute.Login));
+      navigate(AppRoute.Login);
     }
 
     dispatch(updateFavoriteStatus({offerId: id, isFavorite: !isFavorite}));
@@ -19,5 +19,5 @@ const useToggleFavorite = (id: string, isFavorite: boolean) => {
   return handleFavoriteClick;
 };
 
-export { useToggleFavorite };
+export { useToggleFavorite};
 
